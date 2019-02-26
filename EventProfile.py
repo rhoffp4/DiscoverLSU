5309b3c5f6864995a44c6e05df25410e0c8de980
azimme15 <47795157+azimme15@users.noreply.github.com>
2019-02-26 14:04:52 -0600

    Add files via upload

---
+++ EventProfile.py
@@ -0,0 +1,42 @@
+
+
+class EventProfile: 
+    def __init__(self):
+        self.Date = int(0)
+        self.Description = str("")
+        self.Location = str("")
+        self.Time = int(0)
+        self.Attendence = str("")
+
+    def getDate(self):
+        return int(self.Date)
+    
+    def getDescription(self):
+        return str(self.Description)
+
+    def getLocation(self):
+        return str(self.Location)
+
+    def getTime(self):
+        return int(self.Time)
+
+    def getAttendence(self):
+        return str(self.Attendence)
+
+
+    def setDate(self,Date = 0):
+        self.Date = Date
+
+    def setDescription(self,Description = 0):
+        self.Description = Description
+
+    def setLocation(self,Location = 0):
+        self.Location = Location
+
+    def setTime(self,Time = 0):
+        self.Time = Time
+
+    def setAttendence(self,Attendence = 0):
+        self.Attendence = Attendence 
+
+
