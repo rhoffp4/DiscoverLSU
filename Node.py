f16e073790bcedba08544c7346fb4c2fe94f7620
Alexmj003 <47488558+Alexmj003@users.noreply.github.com>
2019-03-19 13:22:48 -0500

    Add files via upload

---
+++ Node.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,identifier):
+        self.id = identifier
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
