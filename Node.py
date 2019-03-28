f26a8647971aab3f2bdf20c68db23c3e28be515b
Alexmj003 <47488558+Alexmj003@users.noreply.github.com>
2019-03-28 13:59:31 -0500

    Add files via upload

--- Node.py
+++ Node.py
@@ -1,6 +1,7 @@
+from Student import Student
 class Node:
-    def __init__(self,identifier):
-        self.data = identifier
+    def __init__(self,data):
+        self.data = data
         self.next = None
 
     def getData(self):
@@ -17,17 +18,17 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self,dataval = None):
-        self.headval = dataval
+    def __init__(self,data = None):
+        self.head = data
 
     def setHead(self,node : Node):
-        self.headval = node
+        self.head = node
 
     def listprint(self):
-        printval = self.headval
+        printval = self.head
         while printval is not None:
-            print(printval.dataval)
-            printval = printval.nextval
+            print(printval.data.getName())
+            printval = printval.next
 
     def Inbetween(self, middle_node, newdata):
         if middle_node is None:
@@ -35,42 +36,124 @@ class LinkedList:
             return
 
         NewNode: Node = Node(newdata)
-        NewNode.nextval = middle_node.nextval
-        middle_node.nextval = NewNode
+        NewNode.next = middle_node.next
+        middle_node.next = NewNode
 
     def Atbegining(self, data_in):
-         NewNode = Node(data_in)
-         NewNode.next = self.head
-         self.head = NewNode
+        NewNode = Node(data_in)
+        NewNode.next = self.head
+        self.head = NewNode
 
-    def RemoveNode(self, Removekey):
+    def RemoveNodebyName(self, Removekey):
+        CurrentNode = self.head
+        PrevNode = Node("Arbitrary")
+        if (self.head.data.getName() == Removekey):
+            self.head = self.head.next
+            CurrentNode.data = None
+            CurrentNode = None
 
-        HeadVal = self.head
-
-        if (HeadVal is not None):
-            if (HeadVal.data == Removekey):
-                self.head = HeadVal.next
-                HeadVal = None
+        while CurrentNode is not None:
+            PrevNode = CurrentNode
+            CurrentNode = CurrentNode.next
+            if (CurrentNode.data.getName() == Removekey):
+                PrevNode.next = CurrentNode.next
+                CurrentNode = None
                 return
 
-        while (HeadVal is not None):
-            if HeadVal.data == Removekey:
-                break
-            prev = HeadVal
-            HeadVal = HeadVal.next
+            if (CurrentNode.next is None):
+                print("Target value not in list")
+                return
 
-        if (HeadVal == None):
-            return
 
-        prev.next = HeadVal.next
-        HeadVal = None
-        
+
     def AtEnd(self, newdata):
             NewNode = Node(newdata)
-            if self.headval is None:
-                self.headval = NewNode
+            if self.head is None:
+                self.head = NewNode
                 return
-            laste = self.headval
-            while (laste.nextval):
-                laste = laste.nextval
-            laste.nextval = NewNode
+
+            last = self.head
+            while (last.next is not None):
+                last = last.next
+
+            last.next = NewNode
+
+    def FindNodeByName(self,TargetValue = str("")):
+        flag = False
+        compareValue = self.head
+        while (flag == False):
+            if(compareValue.data.getName() == TargetValue):
+                flag = True
+                return compareValue.data
+
+            if (compareValue.next is not None):
+                compareValue = compareValue.next
+            else:
+                print("data does not exist")
+
+    def FindAllByMajor(self,TargetMajor = str("")):
+        ReturnList = []
+        TraversalNode = self.head
+        while (TraversalNode is not None):
+            if( TraversalNode.data.getMajor().upper() == TargetMajor.upper()):
+                ReturnList.append(TraversalNode.data)
+
+            TraversalNode = TraversalNode.next
+
+        return ReturnList
+
+
+
+
+
+def DisplayAllByTarget(TargetList = LinkedList()):  # BE CAReFUL when using this, can return either a list or a student class or a string
+    targetinput = input("Enter a selection for Searching [ Name ][ Major ]")
+    if (targetinput.upper() == "NAME"):
+        secondinput = input("Enter a Name: ")
+        studentreturnvalue = TargetList.FindNodeByName(secondinput)
+        if (studentreturnvalue is not None):
+            print(studentreturnvalue.getName())
+            print(studentreturnvalue.getEmail())
+            print(studentreturnvalue.getMajor)
+
+        else:
+            print("Invalid Input")
+
+    if (targetinput.upper() == "MAJOR"):
+        valuelist = TargetList.FindAllByMajor(targetinput)
+        for profile in valuelist:
+            print(profile.getName())
+            print(profile.getEmail())
+            print(profile.getMajor())
+
+    else:
+        print("INvalid input")
+
+
+def __main__():
+    print("Hello")
+    StudentA = Student()
+    StudentA.setName("Alex")
+    print(StudentA.getName())
+    StudentB = Student()
+    StudentB.setName("Sam")
+    print(StudentB.getName())
+    print(StudentA.getName())
+    #Linked List testing
+    List = LinkedList()
+    HeadNode = Node(StudentA)
+    List.setHead(HeadNode)
+    List.listprint()
+    List.AtEnd(StudentB)
+    List.listprint()
+    StudentC = List.FindNodeByName("Sam")
+    print(StudentC.getName())
+    List.RemoveNodebyName("Sam")
+    List.listprint()
+    List.AtEnd(StudentB)
+    List.AtEnd(StudentC)
+    List.listprint()
+    DisplayAllByTarget(List)
+
+if __name__ == '__main__':
+    __main__()
