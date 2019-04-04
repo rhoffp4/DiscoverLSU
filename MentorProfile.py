58449c03386261409be7c4f04f676f62a5873c88
azimme15 <47795157+azimme15@users.noreply.github.com>
2019-04-04 13:56:08 -0500

    Add files via upload

---
+++ MentorProfile.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MentorProfile.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(411, 358)
+        Form.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"background-color: rgb(231, 231, 231);")
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(110, 50, 125, 21))
+        self.lineEdit.setAutoFillBackground(False)
+        self.lineEdit.setStyleSheet("selection-background-color: rgb(255, 255, 255);")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_2.setGeometry(QtCore.QRect(250, 50, 125, 21))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_3.setGeometry(QtCore.QRect(110, 100, 125, 21))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(32, 51, 36, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(32, 105, 33, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(32, 136, 55, 16))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(32, 190, 61, 16))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(32, 221, 46, 16))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(Form)
+        self.label_6.setGeometry(QtCore.QRect(32, 252, 72, 16))
+        self.label_6.setObjectName("label_6")
+        self.lineEdit_4 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_4.setGeometry(QtCore.QRect(110, 130, 125, 21))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.lineEdit_5 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_5.setGeometry(QtCore.QRect(250, 130, 125, 21))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.label_7 = QtWidgets.QLabel(Form)
+        self.label_7.setGeometry(QtCore.QRect(120, 160, 56, 16))
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(Form)
+        self.label_8.setGeometry(QtCore.QRect(250, 160, 57, 16))
+        self.label_8.setObjectName("label_8")
+        self.label_9 = QtWidgets.QLabel(Form)
+        self.label_9.setGeometry(QtCore.QRect(250, 80, 57, 16))
+        self.label_9.setObjectName("label_9")
+        self.label_10 = QtWidgets.QLabel(Form)
+        self.label_10.setGeometry(QtCore.QRect(120, 80, 56, 16))
+        self.label_10.setObjectName("label_10")
+        self.lineEdit_6 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_6.setGeometry(QtCore.QRect(114, 190, 125, 21))
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.lineEdit_7 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_7.setGeometry(QtCore.QRect(114, 221, 125, 21))
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.lineEdit_8 = QtWidgets.QLineEdit(Form)
+        self.lineEdit_8.setGeometry(QtCore.QRect(114, 252, 125, 21))
+        self.lineEdit_8.setObjectName("lineEdit_8")
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(340, 170, 51, 32))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "Name"))
+        self.label_2.setText(_translate("Form", "Email"))
+        self.label_3.setText(_translate("Form", "Students"))
+        self.label_4.setText(_translate("Form", "University"))
+        self.label_5.setText(_translate("Form", "College"))
+        self.label_6.setText(_translate("Form", "Department"))
+        self.label_7.setText(_translate("Form", "Last"))
+        self.label_8.setText(_translate("Form", "First"))
+        self.label_9.setText(_translate("Form", "First"))
+        self.label_10.setText(_translate("Form", "Last"))
+        self.pushButton.setText(_translate("Form", "Add"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
