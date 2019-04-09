fe6ae0b1f8a666b7278956d8ee1f6482651762b3
15sborne <47795176+15sborne@users.noreply.github.com>
2019-04-09 14:33:51 -0500

    Update MentorProfile.py

--- MentorProfile.py
+++ MentorProfile.py
@@ -1,11 +1,5 @@
 # Samuel Okoye
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'MentorProfile.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
+# Susan Borne
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
