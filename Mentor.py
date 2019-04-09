d050e27025e4d4834b4347758254780926f276bf
Alexmj003 <47488558+Alexmj003@users.noreply.github.com>
2019-04-09 13:58:51 -0500

    Update Mentor.py

--- Mentor.py
+++ Mentor.py
@@ -1,49 +1,140 @@
+'''
+Faculty Class for Lsu Discover
+created by _____________(FIll IN LATER)
+v1.0
+v1.1 changelog
+students data changed to list
+new method for adding students
+new method for deleting students
+new method for fetching student list
+Faculty Class object for LSU Discover Application
+'''
 class Faculty:      #class name
     def __init__(self): #Constructor method
-        self.name = str("")
-        self.email = str("")
-        self.students = str("")
-        self.university = str("")
-        self.college = str("")
-        self.department = str("")
+        self.name = str("EMPTY")
+        self.email = str("EMPTY")
+        self.students = []
+        self.university = str("EMPTY")
+        self.college = str("EMPTY")
+        self.department = str("EMPTY")
 
-    def getName(self): # Getter method
-        return str(self.name)
+    def setname(self,name = str("")): # Method to set Student Name
+        if isinstance(name,str):
+            self.name = name
 
-    def getEmail(self): #Getter method
-        return str(self.email)
+    def getname(self): # Getter method
+        if self.name is not None:
+           return self.name
 
-    def getStudents(self): #Getter method
-        return str(self.students)
+    def setemail(self,email = str("")): # setter method for email
+        if isinstance(email,str):
+            self.email = email
 
-    def getUniversity(self): #Getter method
-        return str(self.university)
+    def getemail(self): #Getter method
+        if self.email is not None:
+            return self.email
 
-    def getCollege(self): #Getter method
-        return str(self.college)
+    def addstudent(self,studentname = str("")):
+        if isinstance(studentname,str):
+            self.students.append(studentname)
+            self.students.sort()
 
-    def getDepartment(self): #Getter method
-        return str(self.department)
+    def deletestudent(self,targetname = str("")):
+        if isinstance(targetname,str):
+            self.students.remove(targetname)
 
-    def setName(self, name = ""): #Setter method
-        self.name = str(name)
+    def getstudents(self):
+        if self.students is not None:
+            return self.students
 
-    def setEmail(self, email = ""): #Setter method
-        self.email = str(email)
+    def setuniversity(self,uni = str("")):
+        if isinstance(uni,str):
+            self.university = uni
 
-    def setStudents(self, students = ""): #Setter method
-        self.students = str(students)
+    def getuniversity(self):
+        if self.university is not None:
+            return self.university
 
-    def setUniversity(self, university = ""): #Setter method
-        self.university = str(university)
+    def setcollege(self,college = str("")):
+        if isinstance(college,str):
+            self.college = college
 
-    def setCollege(self, college = ""): #Setter method
-        self.college = str(college)
+    def getcollege(self):
+        if self.college is not None:
+            return self.college
 
-    def setDepartment(self, department = ""): #Setter method
-        self.department = str(department)        
+    def setdepartment(self,department):
+        if isinstance(department,str):
+            self.department = department
 
-a = Faculty()
-a.setEmail("azimme9@lsu.edu")
-print(a)
+    def getdepartment(self):
+        if self.department is not None:
+            return self.department
 
+    #ToSting Method created for storage.py
+    @property
+    def tostring(self):
+        returnstring: str = ""
+        if self.name is not None:
+            returnstring += (self.name + "\n")
+
+        else:
+            returnstring += ("EMPTY\n")
+
+        if self.email is not None:
+            returnstring += (self.email + "\n")
+
+        else:
+            returnstring += ("EMPTY\n")
+
+        if self.students is not None:
+            count = 0
+            for studentname in self.students:
+                count += 1
+
+            returnstring += str(count) + "\n"
+            for studentname in self.students:
+                returnstring += (studentname+"\n")
+
+        else:
+            returnstring += ("0\n")
+
+        if self.university is not None:
+            returnstring += (self.university + "\n")
+
+        else:
+            returnstring += ("EMPTY\n")
+
+        if self.college is not None:
+            returnstring += (self.college + "\n")
+
+        else:
+            returnstring += ("EMPTY\n")
+
+        if self.department is not None:
+            returnstring += (self.department + "\n")
+
+        else:
+            returnstring += ("EMPTY\n")
+
+        return str(returnstring)
+
+
+
+def __main__():
+    alex = Faculty()
+    alex.setname("Alex Mensen-Johnson")
+    alex.setemail("amense1@lsu.edu")
+    alex.addstudent("Sam")
+    alex.addstudent("alex")
+    alex.addstudent("susan")
+    alex.deletestudent("Sam")
+    alex.setuniversity("LSU")
+    alex.setcollege("Engineering")
+    alex.setdepartment("Computer Science")
+    print(alex.tostring)
+    pujan = Faculty()
+    print(pujan.tostring)
+
+if __name__ == '__main__':
+    __main__()
